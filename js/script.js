de6f1c41b7544503144077ed24b49a00cf0c6b17
Juan Martin Zolezzi <juanmartinzolezzi@gmail.com>
2023-08-21 17:09:01 -0300

    falta api

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* menu hamburguer */
+
 $(document).ready(function(){
 
     $('#menu').click(function(){
@@ -11,3 +13,26 @@ $(document).ready(function(){
     });
 
 });
+
+
+/* form */
+let formu = document.getElementById('formulario');
+let datos = []
+const form = (e) => {
+    e.preventDefault()
+    let info = {
+        name: formu.name.value,
+        email: formu.email.value,
+        password: formu.password.value,
+        textarea: formu.textarea.value,
+    }
+
+    datos.push (info);
+    console.log(datos);
+
+    let datosJSON = JSON.stringify(datos);
+    localStorage.setItem('formularioData', datosJSON);
+}
+
+formulario.onsubmit = form;
+
